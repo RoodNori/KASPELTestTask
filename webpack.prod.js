26b8cd1680972332df11ba945858c3a45989d477
roodnori <sinister.sobakin@gmail.com>
2025-07-01 23:11:27 +0300

    fix: собран проект для размещения на github pages

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,11 @@
+const path = require('path');
 const { merge } = require('webpack-merge');
 const common = require('./webpack.config');
 
 module.exports = merge(common, {
   output: {
     filename: 'bundle.js',
-    path: './dist',
+    path: path.resolve(__dirname, './dist'),
   },
   mode: 'production',
   devtool: false,
